b32e6a58ec4d523eebee139f59da7e4b7c12066c
Naveed <144626712+WanderingWalnut@users.noreply.github.com>
2024-07-27 15:27:54 -0600

    setup landing page

--- openai-assistants-quickstart/app/page.tsx
+++ openai-assistants-quickstart/app/page.tsx
@@ -4,24 +4,18 @@ import React from "react";
 import styles from "./page.module.css";
 
 const Home = () => {
-  const categories = {
-    "Basic chat": "basic-chat",
-    "Function calling": "function-calling",
-    "File search": "file-search",
-    All: "all",
-  };
-
   return (
     <main className={styles.main}>
       <div className={styles.title}>
-        Explore sample apps built with Assistants API
+        Welcome to FAMmunity! 🎉
+      </div>
+      <div className={styles.subtitle}>
+        Connecting Families, Empowering Journeys.
       </div>
       <div className={styles.container}>
-        {Object.entries(categories).map(([name, url]) => (
-          <a key={name} className={styles.category} href={`/examples/${url}`}>
-            {name}
-          </a>
-        ))}
+        <a className={styles.category} href="/examples/all">
+          Begin Your Journey
+        </a>
       </div>
     </main>
   );
