65dd49bb88ee4baf0f7924ddcb6366f3ba13fe18
Naveed <144626712+WanderingWalnut@users.noreply.github.com>
2024-07-27 22:19:17 -0600

    Added more to the landing page

--- openai-assistants-quickstart/app/page.tsx
+++ openai-assistants-quickstart/app/page.tsx
@@ -7,13 +7,18 @@ const Home = () => {
   return (
     <main className={styles.main}>
       <div className={styles.title}>
-        Welcome to FAMmunity! 🎉
+        FAMmunity
       </div>
       <div className={styles.subtitle}>
-        Connecting Families, Empowering Journeys.
+        <b>Connecting Families, Empowering Journeys.</b>
+        <br></br>
+        Alberta's Premier Family Support Network.
+      </div>
+      <div className={styles.textContent}>
+      Welcome to FAMmunity, a platform designed to help families navigate the challenges of everyday life. Our AI-powered assistants are here to provide personalized support, guidance, and resources to help you and your loved ones thrive. Whether you're looking for advice on parenting, education, health, or more, FAMmunity is here to help.
       </div>
       <div className={styles.container}>
-        <a className={styles.category} href="/examples/all">
+        <a className={styles.button} href="/examples/all">
           Begin Your Journey
         </a>
       </div>
