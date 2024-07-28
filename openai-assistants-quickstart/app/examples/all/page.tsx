879a4aeab2d28c6c0faa8129a6b33d4c4927f24b
Saad Sheikh <sheikh.saad.aqeel@gmail.com>
2024-07-28 02:03:11 -0600

    arrow back

--- openai-assistants-quickstart/app/examples/all/page.tsx
+++ openai-assistants-quickstart/app/examples/all/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
+import { useRouter } from "next/router";
 import styles from "./page.module.css";
 import Chat from "../../components/chat";
 import FileViewer from "../../components/file-viewer";
 import HelpLineWidget from "../../components/helpline";
 
 const FunctionCalling = () => {
-
   return (
     <main className={styles.main}>
       <div className={styles.container}>
+        <a className={styles.backButton} href="/">
+          &#8592;
+        </a>
         <div className={styles.column}>
           <HelpLineWidget/>
           <FileViewer />
